e8ab3f0f2850ac06442586a0b2da569f8eed38a3
Softmaks <maxviruk@gmail.com>
2024-06-24 00:25:41 +0300

    1
    1

--- JS/task 3.js
+++ JS/task 3.js
@@ -1,53 +1,58 @@
-Задача 3. Сортування за кількістю друзів
+// Задача 3. Сортування за кількістю друзів
 
-Виконуй це завдання у файлі task-3.js
+// Виконуй це завдання у файлі task-3.js
 
+// Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
+// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
 
-Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+// Залиш цей код для перевірки ментором.
 
-Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
+// На що буде звертати увагу ментор при перевірці:
 
-Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-
+// Оголошена змінна sortByDescendingFriendCount
+// Змінній sortByDescendingFriendCount присвоєна стрілочна функція з параметром (users)
+// Для перебирання параметра users використаний метод toSorted()
+// Виклик функції із зазначеним масивом users повертає новий масив користувачів, відсортований за спаданням кількості їхніх друзів
+// Виклик функції з випадковими, але валідними аргументами повертає правильне значення
 
 console.log(
   sortByDescendingFriendCount([
     {
       name: "Moore Hensley",
       friends: ["Sharron Pace"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Sharlene Bush",
       friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
+      gender: "female",
     },
     {
       name: "Ross Vazquez",
       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Elma Head",
       friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
+      gender: "female",
     },
     {
       name: "Carey Barr",
       friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Blackburn Dotson",
       friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Sheree Anthony",
       friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
+      gender: "female",
+    },
   ])
 );
 // [
@@ -87,15 +92,3 @@ console.log(
 //     gender: "male"
 //   }
 // ]
-
-Залиш цей код для перевірки ментором.
-
-
-
-На що буде звертати увагу ментор при перевірці:
-
-Оголошена змінна sortByDescendingFriendCount
-Змінній sortByDescendingFriendCount присвоєна стрілочна функція з параметром (users)
-Для перебирання параметра users використаний метод toSorted()
-Виклик функції із зазначеним масивом users повертає новий масив користувачів, відсортований за спаданням кількості їхніх друзів
-Виклик функції з випадковими, але валідними аргументами повертає правильне значення
